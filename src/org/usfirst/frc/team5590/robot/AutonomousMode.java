ec423186cfb5282bb23b14e5ce13318fb0634864
Dan Starner <dstarner@sjci.com>
2015-03-27 09:22:07 -0400

    Finished Code v2
    Finished Auto code

--- src/org/usfirst/frc/team5590/robot/AutonomousMode.java
+++ src/org/usfirst/frc/team5590/robot/AutonomousMode.java
@@ -5,68 +5,114 @@ import java.util.TimerTask;
 
 import org.usfirst.frc.team5590.robot.Robot;
 
-
 public class AutonomousMode {
 	Timer timer;
 	Robot robot;
-	
-	public AutonomousMode (Robot robo) {
+	private final int EXTEND_COUNT = 110;
+	private final int LIFT_COUNT = EXTEND_COUNT + 60;
+	private final int ROTATE_COUNT = LIFT_COUNT + 55;
+	private final int DRIVE_COUNT = ROTATE_COUNT + 240;
+	private final int LOWER_COUNT = DRIVE_COUNT + 40;
+
+	public AutonomousMode(Robot robo) {
 		robot = robo;
 	}
-    
-	public boolean autoTask(int counter) {
-		if (counter <= 750) {
-			if (counter <= 50) {
-				//Slide out drawer
-				//robot.slider.setX(1);
-			} else if (counter<=90){
-				//lift tote
-				RobotMap.liftController.set(-1);
-			} else if (counter <= 123){
-				//rotate right
-				RobotMap.liftController.set(0);
-				robot.drivetrain.rotateRight(-.58);
-			} else if(counter <= 373) {
-				//drive into zone 3 seconds
-				robot.drivetrain.setSpeed(.5);
-			} else if (counter <= 413){
-				//lower
-				robot.drivetrain.stop();
-				RobotMap.liftController.set(1);
+
+	public boolean autoTask(int type, int counter) {
+		switch (type) {
+		//DRIVE STRAIGHT BACK Over flat
+		case 0:
+			if (counter <= 750) {
+				if (counter <= EXTEND_COUNT) {
+					// Slide out drawer
+					robot.slider.setX(1);
+				} else if (counter <= LIFT_COUNT) {
+					// lift tote
+					robot.slider.setX(0);
+					RobotMap.liftController.set(-1);
+				} else if (counter <= LIFT_COUNT + 380) {
+					//drive backwards
+					RobotMap.liftController.set(0);
+					robot.drivetrain.setSpeed(-.41);
+				} else if (counter <= LIFT_COUNT + 430) {
+					robot.drivetrain.stop();
+					RobotMap.liftController.set(1);
+				} else {
+					robot.drivetrain.stop();
+					RobotMap.liftController.set(0);
+				}
+				return true;
 			} else {
-				robot.drivetrain.stop();
-				RobotMap.liftController.set(0);
+				return false;
 			}
-			return true;
-		} else {
-			return false;
+		//Drive Back straight over bump
+		case 1:
+			if (counter <= 750) {
+				if (counter <= EXTEND_COUNT) {
+					// Slide out drawer
+					robot.slider.setX(1);
+				} else if (counter <= LIFT_COUNT) {
+					// lift tote
+					robot.slider.setX(0);
+					RobotMap.liftController.set(-1);
+				} else if (counter <= LIFT_COUNT + 250) {
+					//drive backwards
+					RobotMap.liftController.set(0);
+					robot.drivetrain.setSpeed(-.41);
+				} else if (counter <= LIFT_COUNT + 300) {
+					robot.drivetrain.stop();
+					RobotMap.liftController.set(1);
+				} else {
+					robot.drivetrain.stop();
+					RobotMap.liftController.set(0);
+				}
+				return true;
+			} else {
+				return false;
+			}
+		//Just drive itself in
+		default:
+			if (counter <= 750) {
+				if (counter <= 250) {
+					// Slide out drawer
+					robot.drivetrain.setSpeed(.5);
+				} else {
+					robot.drivetrain.stop();
+					RobotMap.liftController.set(0);
+				}
+				return true;
+			} else {
+				return false;
+			}
+
 		}
 	}
-	
+
 	public void auto(int seconds, String command, double speed) {
 		timer = new Timer();
-		//Creates task for set number of seconds
+		// Creates task for set number of seconds
 		timer.schedule(new TaskFinisher(), seconds * 1000);
-		//Put run code in here
+		// Put run code in here
 		switch (command) {
-			case "speed": 
-				robot.drivetrain.setSpeed(speed);
-				break;
-			case "turnRight":
-				robot.drivetrain.rotateRight(speed);
-				break;
-			case "turnLeft":
-				robot.drivetrain.rotateLeft(speed);
-			case "stop":
-				robot.drivetrain.stop();
+		case "speed":
+			robot.drivetrain.setSpeed(speed);
+			break;
+		case "turnRight":
+			robot.drivetrain.rotateRight(speed);
+			break;
+		case "turnLeft":
+			robot.drivetrain.rotateLeft(speed);
+		case "stop":
+			robot.drivetrain.stop();
 		}
 	}
-	//Don't touch this!!!!!!!!!! 
+
+	// Don't touch this!!!!!!!!!!
 	class TaskFinisher extends TimerTask {
 		public void run() {
 			System.out.format("Timer Task Finished..!%n");
 			timer.cancel(); // Terminate the timer thread
 		}
 	}
- 
+
 }
--- src/org/usfirst/frc/team5590/robot/Robot.java
+++ src/org/usfirst/frc/team5590/robot/Robot.java
@@ -1,6 +1,4 @@
-
 package org.usfirst.frc.team5590.robot;
-
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -16,229 +14,247 @@ import edu.wpi.first.wpilibj.Victor;
  */
 public class Robot extends IterativeRobot {
 	public OI oi;
-	//Set a run count to see how many times autonomous runs
+	// Set a run count to see how many times autonomous runs
 	static int runCount = 0;
-	//Initialze subsystems
+	// Initialze subsystems
 	public VerticalLift vertLift;
 	public Drivetrain drivetrain;
 	public Slides slider;
 	public Noodle noodle;
-	boolean servoLift = false, servoDrop = false, servoLReset = false, servoDReset = false, servoAllReset = false;
+	boolean servoLift = false, servoDrop = false, servoLReset = false,
+			servoDReset = false, servoAllReset = false;
 	boolean liftUp = false, liftDown = false;
 	public int pos = -1;
 	int autoCount = 0;
 	boolean autoGo = true;
+	private int lift;
 	public AutonomousMode autoCommand;
+	public final int AUTO_VERSION = 0;
+
 	/**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public void robotInit() {
-    	RobotMap.init();
-    	oi = new OI();
-    	drivetrain = new Drivetrain();
-    	vertLift = new VerticalLift();
-    	slider = new Slides();
-    	noodle = new Noodle();
-    	slider.init();
-    	vertLift.init();
-    	
-    }
-    
-    public void autonomousInit() {
-    	autoCommand = new AutonomousMode(this);
-    	autoCount = 0;
-    }
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	public void robotInit() {
+		RobotMap.init();
+		oi = new OI();
+		drivetrain = new Drivetrain();
+		vertLift = new VerticalLift();
+		slider = new Slides();
+		noodle = new Noodle();
+		slider.init();
+		vertLift.init();
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-    	//In theory this should work....if not...well then shit...good luck
-    	//If no speed value, then put -1
-    	if (autoGo = true) {
-    		autoGo = autoCommand.autoTask(autoCount);
-    	} else {
-    		autoGo = false;
-    	}
-    	autoCount++;
-    	
-    }
+	}
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-    	//DRIVE ROBOT
-    	//Figure out speed of tracks from YAxis inputs
-    	double left, right, trigger;
-    	left = oi.xbox.getRawAxis(1)*-1;
-        right = oi.xbox.getRawAxis(5)*-1;
-        //used to be 5 
-        trigger = oi.xbox.getRawAxis(3)*-1;
-    	
-        
-        // Basic drive commands....A makes tracks go equal AKA Straight
-        if (oi.a.get() == true) {
-    		drivetrain.takeJoystickInput(left, left);
-    	} else {
-    		drivetrain.takeJoystickInput(left, right);
-    		
-    	}
-        
-        if (deadzone(trigger)!=0){
-        	drivetrain.takeJoystickInput(trigger, -trigger);
-        }
-        
-        //Rotate
-        //Left trigger == forward...Right trigger is backwards
-    	if (oi.rightB.get() && !oi.a.get()) {
-    		drivetrain.rotateRight(.6);
-    	}
-    	if (oi.leftB.get() && !oi.a.get()) {
-    		drivetrain.rotateLeft(.6);
-    	}
-    	
-    	
-    	//SHHHH (Slalom)
-//    	if (oi.stop.get()) {
-//    		drivetrain.takeJoystickInput(left / 2, left);
-//    	}
-//    	if (oi.start.get()) {
-//    		drivetrain.takeJoystickInput(left, left / 2);
-//    	}
-    	//END SHHH
-    	
-    	//DRIVE ROBOT END
-    	
-    	//DRAWER BEGIN
-    	//Figure out Y Axis of Logitech
-    	double drawAxis;
-    	drawAxis = (oi.logitech.getRawAxis(5));
-    	double twist;
-    	twist = oi.logitech.getRawAxis(2);
-    	
-    	
-    	//USED FOR TESTING SLIDER MANUALLY
-    	
-    	slider.setX(twist);
-    	
-//    	else if (deadzone(drawAxis) > 0) {
-//    		slider.setX(drawAxis);}
-//    	
-//    	else if (deadzone(drawAxis) < 0) {
-//    		slider.setX(drawAxis);}
-//    	
-    	//END MANUAL TEST
-    	
-//    	if (oi.three.get()) {
-//    		slideExtend = true;
-//    	}
-//    	if (oi.four.get()) {
-//    		slideRetract = true;
-//    	}
-//    	
-//    	if (slideExtend) {
-//    		slideExtend = slider.extend();
-//    	} else {
-//    		slider.setX(0);
-//    	}
-//    	
-//    	if (slideRetract) {
-//    		slideRetract = slider.retract();
-//    	} else {
-//    		slider.setX(0);
-//    	}
-//    	
-    	
-    	//DRAWER END
-    	
-    	//VERT LIFT START
-    	
-    	double liftAx = oi.logitech.getRawAxis(1);
-//    	if (deadzone(liftAx) != 0) {
-//    		vertLift.setHeight(deadzone(liftAx));
-//    	}
-    	
-		RobotMap.liftController.set(-liftAx);
-    		
-    	
-    	
-    	if (oi.eleven.get()) { pos = 0;}
-    	
-    	if (oi.twelve.get()) {pos = 1;}
-    	
-    	if (oi.nine.get()) { pos = 2;}
-    	
-    	if (oi.ten.get()) {	pos = 3; }
-    	
-    	if (oi.seven.get()) { pos = 4; }
-    	
-    	if (pos != -1) { //Moves lift mech
-    		pos = vertLift.mover(pos);
-    	}
-    	
-    	
-    	//VERT LIFT END
-    	
-    	//NOODLE START
-    	if (oi.five.get()) {
-    		servoLift = true;
-    	}
-    	if (oi.six.get()) {
-    		servoDrop = true;
-    	}
-    	if (oi.three.get()) {
-    		servoLReset = true;
-    	}
-    	if (oi.four.get()) {
-    		servoDReset = true;
-    	}
-    	if (oi.thumb.get()) {
-    		servoAllReset = true;
-    	}
-    	
-    	
-    	if (servoLift) {
-    		servoLift = noodle.lift();
-    	} 
-    	
-    	if (servoDrop) {
-    		servoDrop = noodle.eject();
-    	} 
-    	
-    	if (servoLReset) {
-    		servoLReset = noodle.resetLift();
-    	} 
-    	
-    	if (servoDReset) {
-    		servoDReset = noodle.resetDrop();
-    	} 
-    	
-    	if (servoAllReset) {
-    		servoAllReset = noodle.resetAll();
-    	} 
-    	
-    	//NOODLE END
-    	
-    	System.out.println(RobotMap.liftLeft.getDistance());
-    	
-   }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-    
-    }
-    
-    //Check for deadband
-    public double deadzone(double axisAmount) {
-    	if (axisAmount < .1 && axisAmount > -.1) {
-    		axisAmount = 0;
-    	} 
-    	return axisAmount;
-    }
-    
-    	
+	public void autonomousInit() {
+		autoCommand = new AutonomousMode(this);
+		autoCount = 0;
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic() {
+		// In theory this should work....if not...well then shit...good luck
+		// If no speed value, then put -1
+		if (autoGo = true) {
+			autoGo = autoCommand.autoTask(AUTO_VERSION, autoCount);
+		} else {
+			autoGo = false;
+		}
+		autoCount++;
+
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+		// DRIVE ROBOT
+		// Figure out speed of tracks from YAxis inputs
+		double left, right, trigger;
+		left = oi.xbox.getRawAxis(1) * -1;
+		right = oi.xbox.getRawAxis(5) * -1;
+		// used to be 5
+		trigger = oi.xbox.getRawAxis(3) * -1;
+
+		// Basic drive commands....A makes tracks go equal AKA Straight
+		if (oi.a.get() == true) {
+			left = triple(left);
+			drivetrain.takeJoystickInput(left, left);
+		} else {
+			drivetrain.takeJoystickInput(left, right);
+
+		}
+
+		if (trigger != 0) {
+			drivetrain.takeJoystickInput(triple(trigger), triple(-trigger));
+		}
+
+		// Rotate
+		// Left trigger == forward...Right trigger is backwards
+		if (oi.rightB.get() && !oi.a.get()) {
+			drivetrain.rotateRight(.6);
+		}
+		if (oi.leftB.get() && !oi.a.get()) {
+			drivetrain.rotateLeft(.6);
+		}
+
+		// SHHHH (Slalom)
+		// if (oi.stop.get()) {
+		// drivetrain.takeJoystickInput(left / 2, left);
+		// }
+		// if (oi.start.get()) {
+		// drivetrain.takeJoystickInput(left, left / 2);
+		// }
+		// END SHHH
+
+		// DRIVE ROBOT END
+
+		// DRAWER BEGIN
+		// Figure out Y Axis of Logitech
+		double drawAxis;
+		drawAxis = (oi.logitech.getRawAxis(5));
+		double twist;
+		twist = -(oi.logitech.getRawAxis(1));
+
+		// USED FOR TESTING SLIDER MANUALLY
+
+		slider.setX(twist);
+
+		// else if (deadzone(drawAxis) > 0) {
+		// slider.setX(drawAxis);}
+		//
+		// else if (deadzone(drawAxis) < 0) {
+		// slider.setX(drawAxis);}
+		//
+		// END MANUAL TEST
+
+		// if (oi.three.get()) {
+		// slideExtend = true;
+		// }
+		// if (oi.four.get()) {
+		// slideRetract = true;
+		// }
+		//
+		// if (slideExtend) {
+		// slideExtend = slider.extend();
+		// } else {
+		// slider.setX(0);
+		// }
+		//
+		// if (slideRetract) {
+		// slideRetract = slider.retract();
+		// } else {
+		// slider.setX(0);
+		// }
+		//
+
+		// DRAWER END
+
+		// VERT LIFT START
+
+		double liftAx = oi.logitech.getRawAxis(1);
+		// if (deadzone(liftAx) != 0) {
+		// vertLift.setHeight(deadzone(liftAx));
+		// }
+
+		// RobotMap.liftController.set(-liftAx);
+
+		RobotMap.liftController.set(lift);
+
+		if (!oi.twelve.get() && !oi.eleven.get()) {
+			lift = 0;
+		}
+		
+		if (oi.eleven.get()) {
+			lift = 1;
+		}
+
+		if (oi.twelve.get()) {
+			lift = -1;
+		}
+
+		if (oi.nine.get()) {
+			pos = 2;
+		}
+
+		if (oi.ten.get()) {
+			pos = 3;
+		}
+
+		if (oi.seven.get()) {
+			pos = 4;
+		}
+
+		if (pos != -1) { // Moves lift mech
+			pos = vertLift.mover(pos);
+		}
+
+		// VERT LIFT END
+
+		// NOODLE START
+		if (oi.five.get()) {
+			servoLift = true;
+		}
+		if (oi.six.get()) {
+			servoDrop = true;
+		}
+		if (oi.three.get()) {
+			servoLReset = true;
+		}
+		if (oi.four.get()) {
+			servoDReset = true;
+		}
+		if (oi.thumb.get()) {
+			servoAllReset = true;
+		}
+
+		if (servoLift) {
+			servoLift = noodle.lift();
+		}
+
+		if (servoDrop) {
+			servoDrop = noodle.eject();
+		}
+
+		if (servoLReset) {
+			servoLReset = noodle.resetLift();
+		}
+
+		if (servoDReset) {
+			servoDReset = noodle.resetDrop();
+		}
+
+		if (servoAllReset) {
+			servoAllReset = noodle.resetAll();
+		}
+
+		// NOODLE END
+
+		System.out.println(RobotMap.liftLeft.getDistance());
+
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testPeriodic() {
+
+	}
+
+	// Check for deadband
+	public double S(double axisAmount) {
+		if (axisAmount < .1 && axisAmount > -.1) {
+			axisAmount = 0;
+		}
+		return axisAmount;
+	}
+	
+	public double triple(double input) {
+		double output = (input * input * input);
+		return output;
+	}
+
 }
